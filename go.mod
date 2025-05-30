ad148b751bcf49b314e567326259768941356f56
patipancorb <162946824+patipancorb@users.noreply.github.com>
2025-05-30 10:10:09 +0700

    u m a

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/popkakkk/my-module-a
 
 go 1.24.3
+
+require github.com/popkakkk/my-module-c v1.6.0
--- util.go
+++ util.go
@@ -1,5 +1,13 @@
 package a_package
 
+import (
+	"fmt"
+
+	c "github.com/popkakkk/my-module-c" // Adjust the import path as necessary
+)
+
 func UtilFunction() string {
+
+	fmt.Println("use package c  --> ", c.UtilFunction())
 	return "This is a utility function from package a"
 }
